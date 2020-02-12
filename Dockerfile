1ef5bfecde9f4c9adfed58ea1bf70a7134cb4778
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2020-02-12 09:55:18 +0100

    Attempt to get subsurface-testdata run ok on Radix

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,8 @@
 FROM webviz/base_image:latest
 
 COPY --chown=appuser . dash_app
+RUN mv ./dash_app/gunicorn_conf.py ./gunicorn_conf.py
+
+CMD gunicorn \
+    --config="./gunicorn_conf.py" \
+    "dash_app.webviz_app:server"
---
+++ gunicorn_conf.py
@@ -0,0 +1,22 @@
+import multiprocessing
+
+# IKNU test
+# For documentation on gunicorn configuration settings, see
+# http://docs.gunicorn.org/en/stable/settings.html
+
+worker_class = 'gthread'
+workers = 10
+threads = 4
+
+max_requests = 20
+worker_tmp_dir = '/dev/shm'
+timeout = 100000
+
+# preload_app = True
+
+bind = '0.0.0.0:5000'
+keepalive = 120
+
+loglevel = 'info'
+accesslog = '-'
+errorlog = '-'
