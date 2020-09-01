40cff687d65db9254592a9a9050f398c05f66fa5
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2020-09-01 08:44:04 +0200

    Completed first version of script

--- hack/publish_app.sh
+++ hack/publish_app.sh
@@ -31,15 +31,44 @@ rm -rf generated_app && webviz build "${WEBVIZ_CONFIG}" --theme equinor --portab
 
 STORAGE_ACCOUNT_NAME="webvizonradix"
 STORAGE_ACCOUNT="https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net"
-CONTAINER_NAME="js-webwiz-hm"
+CONTAINER_NAME="webviz-data"
 
 echo "Copy generated data to storage account"
-az storage blob upload-batch -d "${CONTAINER_NAME}" -s "${SOURCE_FOLDER}" --account-name "${STORAGE_ACCOUNT_NAME}" --account-key "${STORAGE_ACCOUNT_KEY}" --destination-path "${WEBVIZ_STORAGE}"
+az storage blob upload-batch -d "${CONTAINER_NAME}" -s "${SOURCE_FOLDER}" --account-name "${STORAGE_ACCOUNT_NAME}" --account-key "${STORAGE_ACCOUNT_KEY}"
 
 #######################################################################################
-### Modifying the generated assets
+### Using the generated assets
 ###
 
 cp -rf ./generated_app/assets/ ./assets/
 cp ./generated_app/.dockerignore ./.dockerignore
 cp ./generated_app/theme_settings.json ./theme_settings.json
+cp ./generated_app/webviz_app.py ./webviz_app.py
+
+#######################################################################################
+### Using the blob storage plugin
+###
+
+echo "Replacing sections on generated file"
+
+OLD_IMPORT="from webviz_config.webviz_store import WEBVIZ_STORAGE"
+NEW_IMPORT="from webviz_config.webviz_store import WebvizStorage, WEBVIZ_STORAGE\\
+from blob_storage.webviz_blob_store import WEBVIZ_BLOB_STORAGE"
+
+OLD_USE_STORAGE="WEBVIZ_STORAGE.use_storage = True"
+NEW_USE_STORAGE="WEBVIZ_STORAGE.get_stored_data = WEBVIZ_BLOB_STORAGE.get_stored_data\\
+WEBVIZ_STORAGE.use_storage = True"
+
+sed -i -e "s/${OLD_IMPORT}/${NEW_IMPORT}/g" webviz_app.py
+sed -i -e "s/${OLD_USE_STORAGE}/${NEW_USE_STORAGE}/g" webviz_app.py
+
+rm webviz_app.py-e
+
+#######################################################################################
+### Push to github to trigger build
+###
+
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+git add .
+git commit -a -m "Generated code ${current_time}"
+git push
